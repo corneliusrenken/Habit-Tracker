65aa1e8baa177710813eafbb4eed36a8b02b89a5
Cornelius Renken <cornelius.renken@gmail.com>
2022-08-30 07:55:56 -0700

    rename

--- src/checklist-view/ChecklistView.tsx
+++ src/checklist-view/ChecklistView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Checklist from './CheckList';
+import Checklist from './Checklist';
 import { Habit } from '../types';
 import { gridHeightInPx, gridWidthInPx } from '../universalStyling';
 
