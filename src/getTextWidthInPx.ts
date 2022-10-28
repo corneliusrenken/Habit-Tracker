2b70418371ff1eb9baae17a6a60ff26c580b89f1
Cornelius Renken <cornelius.renken@gmail.com>
2022-10-28 11:07:53 -0700

    update getTextWidthInPx to have fontSize parameter

--- src/getTextWidthInPx.ts
+++ src/getTextWidthInPx.ts
@@ -1,9 +1,8 @@
-export default function getTextWidthInPx(text: string | number): number {
+export default function getTextWidthInPx(text: string | number, fontSize: number): number {
   const el = document.createElement('div');
   document.body.appendChild(el);
 
-  el.style.fontFamily = 'DM Sans, sans-serif';
-  el.style.fontSize = `${16}px`;
+  el.style.fontSize = `${fontSize}px`;
   el.style.height = 'auto';
   el.style.width = 'auto';
   el.style.position = 'absolute';
