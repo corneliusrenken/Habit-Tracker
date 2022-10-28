c9d90d44b7f85a16ce6dd64aec5f1309403992ed
Cornelius Renken <cornelius.renken@gmail.com>
2022-10-28 01:52:15 -0700

    refactor to use map to render out components

--- src/Dates.tsx
+++ src/Dates.tsx
@@ -17,13 +17,7 @@ function Dates({ dates, todaysIndex }: DatesProps) {
 
   return (
     <div className="dates-container">
-      <div className="date">{dates[0]}</div>
-      <div className="date">{dates[1]}</div>
-      <div className="date">{dates[2]}</div>
-      <div className="date">{dates[3]}</div>
-      <div className="date">{dates[4]}</div>
-      <div className="date">{dates[5]}</div>
-      <div className="date">{dates[6]}</div>
+      {dates.map((date) => <div className="date">{date}</div>)}
       <div className="date-selector" ref={dateSelector} />
     </div>
   );
--- src/Days.tsx
+++ src/Days.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import './days.css';
 
 function Days() {
+  const days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+
   return (
     <div className="days-container">
-      <div className="day">M</div>
-      <div className="day">T</div>
-      <div className="day">W</div>
-      <div className="day">T</div>
-      <div className="day">F</div>
-      <div className="day">S</div>
-      <div className="day">S</div>
+      {days.map(((day) => <div className="day">{day}</div>))}
     </div>
   );
 }
