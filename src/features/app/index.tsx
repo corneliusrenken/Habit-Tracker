f294b0a9d84bb0f329148fe8a48f617d09f6d241
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-28 10:57:08 -0800

    remove memoized components for now in app index

--- src/features/app/index.tsx
+++ src/features/app/index.tsx
@@ -113,37 +113,6 @@ function App() {
     getSelectedOccurrences(occurrenceData, focusId, dayObject.weekDateStrings[6])
   ), [occurrenceData, dayObject, focusId]);
 
-  const occurrencesComponent = useMemo(() => (
-    <Occurrences
-      displayed={view === 'history' || view === 'focus'}
-      selectedOccurrences={selectedOccurrences}
-    />
-  ), [view, selectedOccurrences]);
-
-  const datesComponent = useMemo(() => (
-    <Dates
-      todaysIndex={dayObject.weekDayIndex}
-      selectedOccurrences={selectedOccurrences}
-    />
-  ), [dayObject, selectedOccurrences]);
-
-  const daysComponent = useMemo(() => (
-    <Days
-      weekDays={dayObject.weekDays}
-      selectedOccurrences={selectedOccurrences}
-    />
-  ), [dayObject, selectedOccurrences]);
-
-  const listComponent = useMemo(() => (
-    <List
-      habits={habits}
-      streaks={streaks}
-      setHabits={setHabits}
-      todaysOccurrences={occurrenceData.dates[dayObject.date] || []}
-      view={latchedListView}
-    />
-  ), [dayObject.date, habits, latchedListView, occurrenceData.dates, streaks]);
-
   useEffect(() => {
     const firstDate = Number(dayObject.weekDateStrings[0].slice(-2));
     const lastDate = Number(dayObject.weekDateStrings[6].slice(-2));
@@ -156,10 +125,33 @@ function App() {
       view={view}
       setView={setViewWrapper}
       bodyHeight={getBodyHeight(view, habits, selectedOccurrences)}
-      occurrences={occurrencesComponent}
-      days={daysComponent}
-      dates={datesComponent}
-      list={listComponent}
+      occurrences={(
+        <Occurrences
+          displayed={view === 'history' || view === 'focus'}
+          selectedOccurrences={selectedOccurrences}
+        />
+      )}
+      days={(
+        <Days
+          weekDays={dayObject.weekDays}
+          selectedOccurrences={selectedOccurrences}
+        />
+      )}
+      dates={(
+        <Dates
+          todaysIndex={dayObject.weekDayIndex}
+          selectedOccurrences={selectedOccurrences}
+        />
+      )}
+      list={(
+        <List
+          habits={habits}
+          streaks={streaks}
+          setHabits={setHabits}
+          todaysOccurrences={occurrenceData.dates[dayObject.date] || []}
+          view={latchedListView}
+        />
+      )}
     />
   );
 }
