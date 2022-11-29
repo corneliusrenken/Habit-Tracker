6766357c2984b034d025dde999470f423411661c
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-28 23:01:46 -0800

    implement client-side function to update habit order

---
+++ src/features/apiFunctions/updateHabitOrder.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { Habit } from '../../globalTypes';
+
+type States = {
+  habits: Habit[] | undefined;
+  setHabits: React.Dispatch<React.SetStateAction<Habit[] | undefined>>;
+};
+
+export default function updateHabitOrder(
+  habitId: number,
+  newOrder: number,
+  states: States,
+) {
+  const { habits, setHabits } = states;
+
+  if (!habits) throw new Error('states should not be undefined');
+
+  const oldOrder = habits.find(({ id }) => id === habitId)?.order;
+
+  if (oldOrder === undefined) throw new Error('habit with this id doesn\'t exist');
+
+  if (oldOrder === newOrder) return;
+
+  if (newOrder < 0 || newOrder >= habits.length) throw new Error('new order value out of range');
+
+  axios({
+    method: 'patch',
+    url: `/api/habits/${habitId}`,
+    data: {
+      order: newOrder,
+    },
+  });
+
+  const orderDifference = newOrder - oldOrder;
+
+  const newHabits: Habit[] = new Array(habits.length);
+
+  habits.forEach((habit) => {
+    const { order } = habit;
+
+    if (order === oldOrder) {
+      newHabits[newOrder] = habit;
+    } else if (orderDifference > 0 && order > oldOrder && order <= newOrder) {
+      newHabits[order - 1] = habit;
+    } else if (orderDifference < 0 && order < oldOrder && order >= newOrder) {
+      newHabits[order + 1] = habit;
+    } else {
+      newHabits[order] = habit;
+    }
+  });
+
+  setHabits(newHabits);
+}
