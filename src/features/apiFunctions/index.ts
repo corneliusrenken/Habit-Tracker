9fede0796b2d5f8645c8bf0b0c9d3d7efed8aca1
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-28 23:23:22 -0800

    create index for apiFunctions

---
+++ src/features/apiFunctions/index.ts
@@ -0,0 +1,15 @@
+import addHabit from './addHabit';
+import removeHabit from './removeHabit';
+import renameHabit from './renameHabit';
+import updateHabitCompleted from './updateHabitCompleted';
+import updateHabitOrder from './updateHabitOrder';
+import updateHabitVisibility from './updateHabitVisibility';
+
+export {
+  addHabit,
+  removeHabit,
+  renameHabit,
+  updateHabitCompleted,
+  updateHabitOrder,
+  updateHabitVisibility,
+};
