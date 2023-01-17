94f243324a0b03d7fb9f45040dc9e17feb272aea
Cornelius Renken <cornelius.renken@gmail.com>
2023-01-17 12:06:18 -0800

    implement createTables function

---
+++ server-sqlite/database/queries/createTables.ts
@@ -0,0 +1,35 @@
+import { Database } from 'better-sqlite3';
+
+export default function createTables(database: Database) {
+  const createHabitsTableStmt = database.prepare(`
+    CREATE TABLE IF NOT EXISTS habits (
+      id INTEGER PRIMARY KEY,
+      name TEXT UNIQUE NOT NULL CHECK (name NOT IN ('')),
+      order_index INTEGER UNIQUE NOT NULL
+    )
+  `);
+
+  const createDaysTableStmt = database.prepare(`
+    CREATE TABLE IF NOT EXISTS days (
+      id INTEGER PRIMARY KEY,
+      date TEXT UNIQUE NOT NULL
+    )
+  `);
+
+  const createOccurrencesTableStmt = database.prepare(`
+    CREATE TABLE IF NOT EXISTS occurrences (
+      id INTEGER PRIMARY KEY,
+      visible INTEGER NOT NULL CHECK (visible IN (0, 1)) DEFAULT 1,
+      complete INTEGER NOT NULL CHECK (complete IN (0, 1)) DEFAULT 0,
+      habit_id INTEGER,
+      day_id INTEGER NOT NULL,
+      UNIQUE (habit_id, day_id),
+      FOREIGN KEY (habit_id) REFERENCES habits (id) ON DELETE SET NULL,
+      FOREIGN KEY (day_id) REFERENCES days (id) ON DELETE CASCADE
+    )
+  `);
+
+  createHabitsTableStmt.run();
+  createDaysTableStmt.run();
+  createOccurrencesTableStmt.run();
+}
--- server-sqlite/index.ts
+++ server-sqlite/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import Database from 'better-sqlite3';
 import { join } from 'path';
+import createTables from './database/queries/createTables';
 
 const app = express();
 
@@ -12,31 +13,10 @@ const db = new Database(dbFilePath);
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
 
-const createTable = db.prepare(`
-  CREATE TABLE IF NOT EXISTS names (
-    id INTEGER PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL
-  );
-`);
-
-createTable.run();
+createTables(db);
 
 app.get('/', (req, res) => {
   res.send('server online!');
-});
-
-app.post('/:name', (req, res) => {
-  const { name } = req.params;
-
-  const addName = db.prepare('INSERT INTO names (name) VALUES (?)');
-
-  try {
-    const { lastInsertRowid } = addName.run(name);
-    console.log('inserted row:', lastInsertRowid);
-    res.status(201).send('added name!');
-  } catch {
-    res.status(500).send('failed to add name :(');
-  }
 });
 
 app.listen(7777, () => console.log(`http://localhost:${7777}`)); // eslint-disable-line no-console
