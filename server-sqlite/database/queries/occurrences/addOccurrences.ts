6ae3352618d1e18982c5e69032f1be32cb643467
Cornelius Renken <cornelius.renken@gmail.com>
2023-01-18 11:34:53 -0800

    implement addOccurrence database function

---
+++ server-sqlite/database/queries/occurrences/addOccurrences.ts
@@ -0,0 +1,13 @@
+import { Database } from 'better-sqlite3';
+
+export default function addOccurrences(database: Database, date: string, habitIds: number[]) {
+  if (habitIds.length === 0) return;
+
+  const insertOccurrencesStmt = database.prepare(`
+    INSERT INTO occurrences (habit_id, day_id)
+    VALUES
+      ${habitIds.map(() => '(?, (SELECT id FROM days WHERE date = ?))').join(',')}
+  `);
+
+  insertOccurrencesStmt.run(...habitIds.flatMap((habitId) => [habitId, date]));
+}
