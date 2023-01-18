f1860e975986abc79e199c8fff73bc413c185e1c
Cornelius Renken <cornelius.renken@gmail.com>
2023-01-18 11:53:05 -0800

    implement addDay database function

---
+++ src/tests__MOVE_TO_SERVER_DIR_LATER/addDay.test.ts
@@ -0,0 +1,35 @@
+import Database from 'better-sqlite3';
+import createTables from '../../server-sqlite/database/queries/createTables';
+import addDay from '../../server-sqlite/database/queries/days/addDay';
+
+let db: Database.Database;
+
+beforeEach(() => {
+  db = new Database(':memory:');
+  createTables(db);
+});
+
+afterEach(() => {
+  db.close();
+});
+
+test('if the date is formatted in any way other than "YYYY-MM-DD, throw an error"', () => {
+  expect(() => addDay(db, '23-01-18')).toThrow('CHECK constraint failed: date IS strftime(\'%Y-%m-%d\', date)');
+  expect(() => addDay(db, '18-01-2023')).toThrow('CHECK constraint failed: date IS strftime(\'%Y-%m-%d\', date)');
+  expect(() => addDay(db, '2023/01/18')).toThrow('CHECK constraint failed: date IS strftime(\'%Y-%m-%d\', date)');
+  expect(() => addDay(db, '20230118')).toThrow('CHECK constraint failed: date IS strftime(\'%Y-%m-%d\', date)');
+  expect(() => addDay(db, 'anything')).toThrow('CHECK constraint failed: date IS strftime(\'%Y-%m-%d\', date)');
+});
+
+test('if the days table already contains a day with the passed date, throw an error', () => {
+  addDay(db, '2023-01-18');
+  expect(() => addDay(db, '2023-01-18')).toThrow('UNIQUE constraint failed: days.date');
+});
+
+test('added day appears in the database correctly', () => {
+  addDay(db, '2023-01-18');
+  const getDaysStmt = db.prepare('SELECT date FROM days');
+  const days = getDaysStmt.all();
+  expect(days.length).toBe(1);
+  expect(days[0].date).toBe('2023-01-18');
+});
