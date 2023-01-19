b01a83ea02fbc45faff27f109b07ab72daa6e51d
Cornelius Renken <cornelius.renken@gmail.com>
2023-01-19 12:15:14 -0800

    modified updateHabit test to check that parameters stay unchanged

--- src/tests__MOVE_TO_SERVER_DIR_LATER/updateHabit.test.ts
+++ src/tests__MOVE_TO_SERVER_DIR_LATER/updateHabit.test.ts
@@ -5,14 +5,18 @@ import updateHabit from '../../server-sqlite/database/queries/habits/updateHabit
 import { verifyOrderInListValues } from './helperFunctions';
 
 let db: Database.Database;
-let exerciseHabitId: number;
+let originalHabit: {
+  id: number;
+  name: string;
+  order_in_list: number;
+};
 
 beforeEach(() => {
   db = new Database(':memory:');
   createTables(db);
   const addDayStmt = db.prepare('INSERT INTO days (date) VALUES (?)');
   addDayStmt.run('2023-01-17');
-  exerciseHabitId = addHabit(db, 'exercise', '2023-01-17').id;
+  originalHabit = addHabit(db, 'exercise', '2023-01-17');
 });
 
 afterEach(() => {
@@ -20,8 +24,8 @@ afterEach(() => {
 });
 
 test('doesn\'t throw when the changed value is the same as the original', () => {
-  expect(() => updateHabit(db, exerciseHabitId, { name: 'exercise' })).not.toThrow();
-  expect(() => updateHabit(db, exerciseHabitId, { orderInList: 0 })).not.toThrow();
+  expect(() => updateHabit(db, originalHabit.id, { name: 'exercise' })).not.toThrow();
+  expect(() => updateHabit(db, originalHabit.id, { orderInList: 0 })).not.toThrow();
 });
 
 test('throws an error if the habit to update does not exist', () => {
@@ -32,20 +36,23 @@ test('throws an error if the habit to update does not exist', () => {
 describe('updating name', () => {
   test('throws an error if the new name value already exists', () => {
     addHabit(db, 'read', '2023-01-17');
-    expect(() => updateHabit(db, exerciseHabitId, { name: 'read' })).toThrow('UNIQUE constraint failed: habits.name');
+    expect(() => updateHabit(db, originalHabit.id, { name: 'read' })).toThrow('UNIQUE constraint failed: habits.name');
   });
 
   test('throws an error if the new name is invalid', () => {
     addHabit(db, 'read', '2023-01-17');
-    expect(() => updateHabit(db, exerciseHabitId, { name: '' })).toThrow('CHECK constraint failed: name NOT IN (\'\')');
+    expect(() => updateHabit(db, originalHabit.id, { name: '' })).toThrow('CHECK constraint failed: name NOT IN (\'\')');
   });
 
-  test('updates the habit\'s name', () => {
-    updateHabit(db, exerciseHabitId, { name: 'read' });
+  test('updates the habit\'s name, and only the habit\'s name', () => {
+    updateHabit(db, originalHabit.id, { name: 'read' });
 
-    const getHabitByIdStmt = db.prepare('SELECT name FROM habits WHERE id = ?');
+    const getHabitByIdStmt = db.prepare('SELECT name, order_in_list FROM habits WHERE id = ?');
 
-    expect(getHabitByIdStmt.get(exerciseHabitId).name).toBe('read');
+    const updatedHabit = getHabitByIdStmt.get(originalHabit.id);
+
+    expect(updatedHabit.name).toBe('read');
+    expect(updatedHabit.order_in_list).toBe(originalHabit.order_in_list);
   });
 });
 
@@ -59,26 +66,29 @@ describe('updating order in list', () => {
   });
 
   test('throws an error if the new order in list is out of range (less than 0 or greater than habit length - 1)', () => {
-    expect(() => updateHabit(db, exerciseHabitId, { orderInList: -1 })).toThrow('Error: Order in list is out of range. The value needs to inclusively be between 0 and the count of all habits - 1');
-    expect(() => updateHabit(db, exerciseHabitId, { orderInList: 3 })).toThrow('Error: Order in list is out of range. The value needs to inclusively be between 0 and the count of all habits - 1');
+    expect(() => updateHabit(db, originalHabit.id, { orderInList: -1 })).toThrow('Error: Order in list is out of range. The value needs to inclusively be between 0 and the count of all habits - 1');
+    expect(() => updateHabit(db, originalHabit.id, { orderInList: 3 })).toThrow('Error: Order in list is out of range. The value needs to inclusively be between 0 and the count of all habits - 1');
   });
 
-  test('updates the habit\'s order in list', () => {
-    updateHabit(db, exerciseHabitId, { orderInList: 2 });
+  test('updates the habit\'s order in list, and only the order in list', () => {
+    updateHabit(db, originalHabit.id, { orderInList: 2 });
 
-    const getHabitByIdStmt = db.prepare('SELECT order_in_list FROM habits WHERE id = ?');
+    const getHabitByIdStmt = db.prepare('SELECT name, order_in_list FROM habits WHERE id = ?');
 
-    expect(getHabitByIdStmt.get(exerciseHabitId).order_in_list).toBe(2);
+    const updatedHabit = getHabitByIdStmt.get(originalHabit.id);
+
+    expect(updatedHabit.order_in_list).toBe(2);
+    expect(updatedHabit.name).toBe(originalHabit.name);
   });
 
   test('when updating order in list, all other habit\'s order in list values shift in the opposite direction to keep the uninterrupted order range from 0 -> length of habits - 1', () => {
     // original order: ['exercise', 'read', 'sleep']
-    updateHabit(db, exerciseHabitId, { orderInList: 1 }); // order from 0 -> 1
+    updateHabit(db, originalHabit.id, { orderInList: 1 }); // order from 0 -> 1
     verifyOrderInListValues(db, ['read', 'exercise', 'sleep']);
-    updateHabit(db, exerciseHabitId, { orderInList: 0 }); // revert -> 0
-    updateHabit(db, exerciseHabitId, { orderInList: 2 }); // order from 0 -> 2
+    updateHabit(db, originalHabit.id, { orderInList: 0 }); // revert -> 0
+    updateHabit(db, originalHabit.id, { orderInList: 2 }); // order from 0 -> 2
     verifyOrderInListValues(db, ['read', 'sleep', 'exercise']);
-    updateHabit(db, exerciseHabitId, { orderInList: 0 }); // revert -> 0
+    updateHabit(db, originalHabit.id, { orderInList: 0 }); // revert -> 0
     updateHabit(db, readHabitId, { orderInList: 0 }); // order from 1 -> 0
     verifyOrderInListValues(db, ['read', 'exercise', 'sleep']);
     updateHabit(db, readHabitId, { orderInList: 1 }); // revert -> 1
