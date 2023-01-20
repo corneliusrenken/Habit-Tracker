70119bf29d384221625f1d62ce28fc103bda7cf7
Cornelius Renken <cornelius.renken@gmail.com>
2023-01-20 09:02:38 -0800

    initialized project using electron forge's 'webpack + typescript' & 'react with typescript' guides
    https://www.electronforge.io/templates/typescript-+-webpack-template
    https://www.electronforge.io/guides/framework-integration/react-with-typescript

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(<h1>Hello React</h1>);
