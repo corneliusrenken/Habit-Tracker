a1ebc8fa1a6b98bb9784dd1a619b628a8fe83ac8
Cornelius Renken <cornelius.renken@gmail.com>
2023-01-27 23:10:23 -0800

    max list height indicator now updates dynamically

--- src/features/layout/Indicators.tsx
+++ src/features/layout/Indicators.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type LayoutOptions = {
   minMarginHeight: number;
@@ -6,19 +6,17 @@ type LayoutOptions = {
 };
 
 export default function Indicators({ options }: { options: LayoutOptions }) {
+  useEffect(() => {
+    document.documentElement.style.setProperty('--max-list-height', `${options.maxListHeight}px`);
+  }, [options.maxListHeight]);
+
   return (
     <div>
       <div className="desired-margin-indicator" style={{ height: options.minMarginHeight, top: 0 }} />
       <div className="desired-margin-indicator" style={{ height: options.minMarginHeight, bottom: 0 }} />
       <div className="actual-margin" style={{ height: 'var(--margin-height)', top: 0 }} />
       <div className="actual-margin" style={{ height: 'var(--margin-height)', bottom: 0 }} />
-      <div
-        className="max-list-height-indicator"
-        style={{
-          height: options.maxListHeight,
-          top: (window.innerHeight - options.maxListHeight) / 2,
-        }}
-      />
+      <div className="max-list-height-indicator" />
     </div>
   );
 }
